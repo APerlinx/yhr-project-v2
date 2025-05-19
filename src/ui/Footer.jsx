116b02a591204883ad24e1da15565689efd3f8f2
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-05-20 00:04:06 +0300

    Fix the fact that nagishli.js ruin my UI

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -8,20 +8,22 @@ function Footer() {
 
   return (
     <div className="flex items-center justify-between p-5">
-      <div className="cursor-default">
+      <div className="cursor-default ">
         <h1 className="text-lg leading-tight">
           Herman Architects Ltd
-          <p className="text-xs">
+          <p className="text-center text-xs">
             &copy;{new Date().getFullYear()}, All rights reserved.
           </p>
         </h1>
-
-        <span className="text-xs">
+        <span className="text-xs sm:hidden">
           Website & Design by
           <a href="https://github.com/APerlinx"> Alon Perlin</a>
         </span>
       </div>
-
+      <span className="my-4 hidden text-xs sm:block">
+        Website & Design by
+        <a href="https://github.com/APerlinx"> Alon Perlin</a>
+      </span>
       {/*Footer Tool bar*/}
       <div className="flex flex-col flex-wrap items-center justify-start gap-1 text-base sm:flex-row sm:gap-10 sm:text-lg">
         <div className="block sm:hidden">
